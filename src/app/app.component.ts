7ec3de650bf48b287a234848a4ead1263b1e2f9d
Damien Springuel <dspringuel@vendasta.com>
2019-04-22 07:52:52 -0600

    Event source example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'stream-client';
+  events: string[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log('On init');
+    const stream: EventSource = new EventSource('http://localhost:13000/stream');
+    stream.addEventListener('message', (message: MessageEvent) => {
+      this.events.push(message.data);
+      console.log(message);
+    });
+    stream.addEventListener('streamEnd', (message: MessageEvent) => {
+      this.events.push(`${message.data} (it was the last one)`);
+      stream.close();
+    });
+
+    stream.addEventListener('open', (open: Event) => {
+      console.log(open);
+    });
+    stream.addEventListener('error', (error: Event) => {
+      console.log(error);
+    });
+  }
 }
