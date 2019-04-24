4e50c913244a3fbf2b7a6ad1bcae4e0a39821827
Damien Springuel <dspringuel@vendasta.com>
2019-04-24 09:23:56 -0600

    Add listener

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,18 +14,25 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     console.log('On init');
     const stream: EventSource = new EventSource('http://localhost:13000/stream');
+
     stream.addEventListener('message', (message: MessageEvent) => {
       this.events.push(message.data);
       console.log(message);
     });
+
     stream.addEventListener('streamEnd', (message: MessageEvent) => {
       this.events.push(`${message.data} (it was the last one)`);
       stream.close();
     });
 
+    stream.addEventListener('hello', (message: MessageEvent) => {
+      this.events.push(`Hello Message => ${message.data}`);
+    });
+
     stream.addEventListener('open', (open: Event) => {
       console.log(open);
     });
+
     stream.addEventListener('error', (error: Event) => {
       console.log(error);
     });
